8fc389382f1200897d9c9c0f4238a928bfa18790
Betty Blaize <bethlakshmi@gmail.com>
2019-09-10 01:07:35 +0000

    safe enough - 2 more templates

--- gbe/cms_plugins.py
+++ gbe/cms_plugins.py
@@ -42,14 +42,14 @@ class SubscribeEmailPlugin(CMSPluginBase):
     module = _("GBE Plugins")
     name = _("Subscribe to Email")  # name of the plugin in the interface
     render_template = 'gbe/email_subscribe.tmpl'
-
+'''
 
 class GoFundMePlugin(CMSPluginBase):
     model = CMSPlugin
     module = _("GBE Plugins")
     name = _("Go Fund Me Widget")  # name of the plugin in the interface
     render_template = 'gbe/go_fund_me.tmpl'
-'''
+
 
 class ShareOnFacebookPlugin(CMSPluginBase):
     model = CMSPlugin
@@ -57,26 +57,26 @@ class ShareOnFacebookPlugin(CMSPluginBase):
     name = _("Share on Facebook")  # name of the plugin in the interface
     render_template = 'gbe/facebook_share.tmpl'
 
-'''
+
 class FollowOnFacebookPlugin(CMSPluginBase):
     model = CMSPlugin
     module = _("GBE Plugins")
     name = _("Follow us on Facebook")  # name of the plugin in the interface
     render_template = 'gbe/facebook_follow.tmpl'
 
-
+'''
 class AdRotatorPlugin(CMSPluginBase):
     model = CMSPlugin
     module = _("GBE Plugins")
     name = _("Ad Rotator")
     render_template = 'gbe/ad-rotator.tmpl'
-
-# register the plugins
-plugin_pool.register_plugin(ClassIdeaPlugin)
-plugin_pool.register_plugin(ContactFormPlugin)
-plugin_pool.register_plugin(SubscribeEmailPlugin)
-plugin_pool.register_plugin(GoFundMePlugin)
 '''
+# register the plugins
+#plugin_pool.register_plugin(ClassIdeaPlugin)
+#plugin_pool.register_plugin(ContactFormPlugin)
+#plugin_pool.register_plugin(SubscribeEmailPlugin)
+
+plugin_pool.register_plugin(GoFundMePlugin)
 plugin_pool.register_plugin(ShareOnFacebookPlugin)
-#plugin_pool.register_plugin(FollowOnFacebookPlugin)
+plugin_pool.register_plugin(FollowOnFacebookPlugin)
 #plugin_pool.register_plugin(AdRotatorPlugin)
