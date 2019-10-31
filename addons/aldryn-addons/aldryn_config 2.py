ba516d52cefc5332dfefa685d3aed898a0d9992d
Betty Blaize <bethlakshmi@gmail.com>
2019-10-31 13:56:16 +0000

    where is this 2.py crap coming from???

--- addons/aldryn-addons/aldryn_config 2.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from aldryn_client import forms
-
-
-class Form(forms.BaseForm):
-    def to_settings(self, data, settings):
-        return settings
--- addons/aldryn-common/aldryn_config 2.py
+++
@@ -1,8 +0,0 @@
-from aldryn_client import forms
-
-class Form(forms.BaseForm):
-    paginator_paginate_by = forms.CharField('Paginator: item per page count', required=False)
-
-    def to_settings(self, data, settings):
-        settings['ALDRYN_COMMON_PAGINATOR_PAGINATE_BY'] = data['paginator_paginate_by']
-        return settings
