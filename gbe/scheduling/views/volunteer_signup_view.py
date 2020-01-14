a4e6434b353ad050bbed32bb44b938d1daf61a9b
Betty Blaize <bethlakshmi@gmail.com>
2020-01-13 19:11:00 -0500

    Revert "where did this come from?"
    This reverts commit f2fd2f90710b39742496e5119fd3d07b06960024.

--- gbe/scheduling/views/volunteer_signup_view.py
+++ gbe/scheduling/views/volunteer_signup_view.py
@@ -71,10 +71,13 @@ class VolunteerSignupView(View):
         self.this_day = None
 
         if "day" in self.request.GET:
-            self.this_day = get_object_or_404(
-                ConferenceDay,
-                day=datetime.strptime(self.request.GET.get('day', None),
-                                      URL_DATE))
+            try:
+                self.this_day = get_object_or_404(
+                    ConferenceDay,
+                    day=datetime.strptime(self.request.GET.get('day', None),
+                                          URL_DATE))
+            except ValueError:
+                raise Http404
             self.conference = self.this_day.conference
 
         elif "conference" in self.request.GET:
