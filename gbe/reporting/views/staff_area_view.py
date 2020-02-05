990055dfdcacc674ff13f4f23fd3854aaaeb585b
Betty Blaize <bethlakshmi@gmail.com>
2020-02-05 00:54:53 -0500

    Revert "new feature - show role, and make filter for it."
    This reverts commit 236dda7465cb9b6224968491904eb3390c055e6d.

--- gbe/reporting/views/staff_area_view.py
+++ gbe/reporting/views/staff_area_view.py
@@ -4,7 +4,6 @@ from gbe.functions import validate_perms
 from scheduler.idd import get_occurrences
 from gbe.scheduling.views.functions import show_general_status
 from gbe.models import StaffArea
-from gbetext import role_commit_map
 
 
 def staff_area_view(request, area_id):
@@ -13,35 +12,26 @@ def staff_area_view(request, area_id):
     volunteers scheduled, sorted by time/day
     See ticket #250
     '''
+    show_style = False
     viewer_profile = validate_perms(request, 'any', require=True)
+    if 'area' in request.GET.keys():
+        show_style = (request.GET['area'] == "Show")
 
-    other = "Potential"
-    roles = []
-    if 'filter' in request.GET.keys() and (
-            request.GET['filter'] == "Potential"):
-        other = "Committed"
-    for role, commit in role_commit_map.items():
-        if commit[0] == 1 or (
-                commit[0] > 0 and commit[0] < 4 and other == "Committed"):
-            roles += [role]
     opps_response = None
     area = None
     opps = None
-    conference = None
-    try:
-        area = StaffArea.objects.get(pk=area_id)
-        opps_response = get_occurrences(labels=[
-            area.conference.conference_slug,
-            area.slug])
-        conference = area.conference
-    except StaffArea.DoesNotExist:
+
+    if show_style:
         parent_response = get_occurrences(foreign_event_ids=[area_id])
         if parent_response.occurrences:
             area = parent_response.occurrences[0]
             opps_response = get_occurrences(
                 parent_event_id=parent_response.occurrences[0].pk)
-            conference = area.confitem.e_conference
-
+    else:
+        area = get_object_or_404(StaffArea, pk=area_id)
+        opps_response = get_occurrences(labels=[
+            area.conference.conference_slug,
+            area.slug])
     if opps_response:
         show_general_status(request, opps_response, "staff_area")
         opps = opps_response.occurrences
@@ -49,9 +39,4 @@ def staff_area_view(request, area_id):
     return render(request,
                   'gbe/report/staff_area_schedule.tmpl',
                   {'opps': opps,
-                   'area': area,
-                   'conference': conference,
-                   'role_commit_map': role_commit_map,
-                   'visible_roles': roles,
-                   'other_option': other,
-                   })
+                   'area': area})
