7ba323ce10856fed13185463d27bfbf4a7b352f2
Betty Blaize <bethlakshmi@gmail.com>
2020-02-07 18:41:57 -0500

    Tests on work so far, and separating the parts of display

--- tests/gbe/test_vendor_changestate.py
+++ tests/gbe/test_vendor_changestate.py
@@ -1,4 +1,3 @@
-import nose.tools as nt
 from django.test import TestCase
 from django.test.client import RequestFactory
 from django.test import Client
@@ -21,10 +20,12 @@ class TestVendorChangestate(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.client = Client()
-        self.vendor = VendorFactory()
+        self.vendor = VendorFactory(submitted=True)
         self.privileged_user = ProfileFactory().user_object
         grant_privilege(self.privileged_user, 'Vendor Coordinator')
-        self.data = {'accepted': '3'}
+        grant_privilege(self.privileged_user, 'Vendor Reviewers')
+        self.data = {'accepted': '3',
+                     'level': 'Featured'}
 
     def test_vendor_changestate_authorized_user(self):
         '''The proper coordinator is changing the state, it works'''
@@ -32,8 +33,12 @@ class TestVendorChangestate(TestCase):
                       args=[self.vendor.pk],
                       urlconf='gbe.urls')
         login_as(self.privileged_user, self)
-        response = self.client.post(url, data=self.data)
-        nt.assert_equal(response.status_code, 302)
+        response = self.client.post(url, data=self.data, follow=True)
+        print response
+        self.assertRedirects(response, reverse(
+            "vendor_review_list",
+            urlconf='gbe.urls'))
+        self.assertContains(response, ", Featured")
 
     def test_vendor_changestate_unauthorized_user(self):
         '''A regular user is changing the state, it fails'''
@@ -42,4 +47,4 @@ class TestVendorChangestate(TestCase):
                       urlconf='gbe.urls')
         login_as(ProfileFactory(), self)
         response = self.client.post(url, data=self.data)
-        nt.assert_equal(response.status_code, 403)
+        self.assertEqual(response.status_code, 403)
