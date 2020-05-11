1da48e1d06ea6fca02c498b1a08bd716bb2e9c6a
Betty Blaize <bethlakshmi@gmail.com>
2020-05-10 22:53:14 -0400

    First round of gets

--- tests/gbe/test_edit_email.py
+++ tests/gbe/test_edit_email.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.test import Client
 from django.core.urlresolvers import reverse
-from tests.contexts import VolunteerContext
 from tests.factories.gbe_factories import (
     ProfileFactory,
     ProfilePreferencesFactory,
@@ -18,11 +17,16 @@ from tests.functions.gbe_functions import (
     login_as
 )
 from gbetext import (
+    bad_token_msg,
     default_update_profile_msg,
     email_pref_note,
+    link_sent_msg,
+    send_link_message,
 )
 import os
 from django.contrib.sites.models import Site
+from gbe.email.functions import create_unsubscribe_link
+from django.utils.html import escape
 
 
 class TestEditEmail(TestCase):
@@ -36,27 +40,49 @@ class TestEditEmail(TestCase):
         self.url = reverse(self.view_name, urlconf='gbe.urls')
         os.environ['RECAPTCHA_DISABLE'] = 'True'
 
-    def get_form(self, invalid=False):
-        data = {'email': self.profile.user_object.email,
-                'send_daily_schedule': True,
-                'send_bid_notifications': False,
-                'send_role_notifications': False,
-                'send_schedule_change_notifications': True, }
-        if invalid:
-            del(data['email'])
-        return data
+    def test_update_email_no_token(self):
+        response = self.client.get(self.url)
+        self.assertContains(response, bad_token_msg)
+        self.assertContains(response, "Email:")
 
-    def test_update_email_no_preferences(self):
+    def test_update_email_bad_token(self):
+        self.url = reverse(
+            self.view_name,
+            urlconf='gbe.urls',
+            args=["%s:%s" % (self.profile.user_object.email, "badsignature")])
+        response = self.client.get(self.url)
+        self.assertContains(response, bad_token_msg)
+        self.assertContains(response, "Email:")
+
+    def test_update_email_bad_user(self):
+        self.url = create_unsubscribe_link("emailedit@doesnotexist.com")
+        response = self.client.get(self.url)
+        self.assertContains(response, bad_token_msg)
+        self.assertContains(response, "Email:")
+
+    def test_update_email_good_token(self):
+        self.url = create_unsubscribe_link(
+            self.profile.user_object.email
+            )+ "?email_disable=send_schedule_change_notifications"
+        response = self.client.get(self.url)
+        self.assertContains(response, escape(email_pref_note))
+        self.assertContains(response, "Email Options")
+        self.assertNotContains(response, "Email:")
+        self.assertContains(
+            response,
+            '<input type="checkbox" ' +
+            'name="send_schedule_change_notifications"' +
+            ' id="id_send_schedule_change_notifications" />')
+        self.assertContains(
+            response, "shadow-red")
+
+    def test_update_email_missing_preferences(self):
         profile = ProfileFactory()
-        data = self.get_form()
-        data['email'] = profile.user_object.email
-        print(profile.user_object.email)
-        response = self.client.post(self.url,
-                                    data=self.get_form(),
-                                    follow=True)
-        preferences = ProfilePreferences.objects.get(profile=profile)
-        self.assertTrue(preferences.send_daily_schedule)
-        self.assertFalse(preferences.send_bid_notifications)
+        self.url = create_unsubscribe_link(profile.user_object.email)
+        response = self.client.get(self.url)
+        self.assertContains(response, escape(email_pref_note))
+        self.assertContains(response, "Email Options")
+        self.assertNotContains(response, "Email:")
 
     def tearDown(self):
         del os.environ['RECAPTCHA_DISABLE']
@@ -67,17 +93,6 @@ class TestEditEmail(TestCase):
         self.assertContains(response, 'id="recaptcha-verification"')
         self.assertContains(response, email_pref_note.replace("'", "&#39;"))
 
-    def test_update_email_disable_email(self):
-        url = self.url + "?email_disable=send_schedule_change_notifications"
-        response = self.client.get(url)
-        self.assertContains(
-            response,
-            '<input type="checkbox" ' +
-            'name="send_schedule_change_notifications"' +
-            ' id="id_send_schedule_change_notifications" />')
-        self.assertContains(
-            response, "shadow-red")
-
     def test_update_email_post_valid_form(self):
         response = self.client.post(self.url,
                                     data=self.get_form(),
@@ -87,7 +102,6 @@ class TestEditEmail(TestCase):
         preferences = ProfilePreferences.objects.get(profile=self.profile)
         self.assertTrue(preferences.send_daily_schedule)
         self.assertFalse(preferences.send_bid_notifications)
-        print(response.content)
         assert_alert_exists(
             response, 'success', 'Success', default_update_profile_msg)
 
