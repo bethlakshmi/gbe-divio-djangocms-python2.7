a2a12f272624100470ef5793e08aa13e7e2c868e
Betty Blaize <bethlakshmi@gmail.com>
2020-08-15 13:09:22 -0400

    refactor show display

--- scheduler/models/event.py
+++ scheduler/models/event.py
@@ -200,13 +200,6 @@ class Event(Schedulable):
     def duration(self):
         return self.eventitem.child().sched_duration
 
-    # get castings as in what acts have been booked for this event
-    @property
-    def casting_list(self):
-        return ActResource.objects.filter(
-            allocations__event=self,
-            _item__act__accepted=3).order_by('_item__act__performer__name')
-
     def __str__(self):
         return self.eventitem.describe
 
