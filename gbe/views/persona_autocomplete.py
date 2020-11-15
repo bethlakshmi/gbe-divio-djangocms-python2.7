302cf05d486faf6e4f914de2ed01e480462c1e2d
Betty Blaize <bethlakshmi@gmail.com>
2020-11-15 10:52:37 -0500

    tests

--- gbe/views/persona_autocomplete.py
+++ gbe/views/persona_autocomplete.py
@@ -1,11 +1,12 @@
 from dal import autocomplete
 from django.db.models import Q
 from gbe.models import Persona
+from gbe.functions import validate_profile
 
 
 class PersonaAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
-        if not self.request.user.is_authenticated:
+        if not validate_profile(self.request, require=True):
             return Persona.objects.none()
 
         qs = Persona.objects.filter(
---
+++ tests/gbe/test_profile_autocomplete.py
@@ -0,0 +1,75 @@
+from django.test import TestCase
+from django.test import Client
+from django.urls import reverse
+from tests.factories.gbe_factories import (
+    ProfileFactory,
+    UserFactory,
+)
+from tests.functions.gbe_functions import login_as
+from gbe.functions import validate_profile
+
+
+class TestProfileAutoComplete(TestCase):
+    url = reverse('profile-autocomplete')
+
+    def setUp(self):
+        self.client = Client()
+        self.user = ProfileFactory()
+
+    def test_list_profile(self):
+        profile = ProfileFactory()
+        login_as(self.user, self)
+        response = self.client.get(self.url)
+        self.assertContains(response, profile.display_name)
+        self.assertContains(response, profile.pk)
+
+    def test_no_access_profile(self):
+        profile = ProfileFactory()
+        response = self.client.get(self.url)
+        self.assertNotContains(response, profile.display_name)
+        self.assertNotContains(response, profile.pk)
+
+    def test_list_profile_w_search_by_display_name(self):
+        profile1 = ProfileFactory()
+        profile2 = ProfileFactory()
+        login_as(self.user, self)
+        response = self.client.get("%s?q=%s" % (
+            self.url,
+            profile1.display_name))
+        self.assertContains(response, profile1.display_name)
+        self.assertContains(response, profile1.pk)
+        self.assertNotContains(response, profile2.display_name)
+
+    def test_list_profile_w_search_by_username(self):
+        profile1 = ProfileFactory()
+        profile2 = ProfileFactory()
+        login_as(self.user, self)
+        response = self.client.get("%s?q=%s" % (
+            self.url,
+            profile1.user_object.username))
+        self.assertContains(response, profile1.display_name)
+        self.assertContains(response, profile1.pk)
+        self.assertNotContains(response, profile2.display_name)
+
+    def test_list_profile_w_search_by_first_name(self):
+        profile1 = ProfileFactory()
+        profile2 = ProfileFactory()
+        login_as(self.user, self)
+        response = self.client.get("%s?q=%s" % (
+            self.url,
+            profile1.user_object.first_name))
+        self.assertContains(response, profile1.display_name)
+        self.assertContains(response, profile1.pk)
+        self.assertNotContains(response, profile2.display_name)
+
+    def test_list_profile_w_search_by_last_name(self):
+        profile1 = ProfileFactory()
+        profile2 = ProfileFactory(user_object=UserFactory(
+            last_name="NOT HERE"))
+        login_as(self.user, self)
+        response = self.client.get("%s?q=%s" % (
+            self.url,
+            profile1.user_object.last_name))
+        self.assertContains(response, profile1.display_name)
+        self.assertContains(response, profile1.pk)
+        self.assertNotContains(response, profile2.display_name)
