216e2bc932bb2c8964204ae495a7a1a3d4a4c115
Betty Blaize <bethlakshmi@gmail.com>
2021-02-27 19:42:31 -0500

    refactor

--- gbe/models/performer.py
+++ gbe/models/performer.py
@@ -10,7 +10,6 @@ from django.db.models import (
 from gbe.models import Profile
 from scheduler.models import WorkerItem
 from filer.fields.image import FilerImageField
-from django.urls import reverse
 
 
 class Performer (WorkerItem):
@@ -40,10 +39,6 @@ class Performer (WorkerItem):
         null=True,
         related_name="image_performer")
     festivals = TextField(blank=True)     # placeholder only
-
-    # used by CreateView and UpdateView
-    def get_absolute_url(self):
-        return reverse('home', urlconf="gbe.urls")
 
     def get_profiles(self):
         '''
--- gbe/views/make_performer_view.py
+++ gbe/views/make_performer_view.py
@@ -2,11 +2,16 @@ from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import AccessMixin, PermissionRequiredMixin
 from gbe.models import Performer
 from gbe.forms import PersonaForm
+from django.urls import reverse_lazy
+
 
 class PerformerCreate(PermissionRequiredMixin, AccessMixin, CreateView):
     model = Performer
     form_class = PersonaForm
-    template_name = 'gbe/modal_form.tmpl'
+    template_name = 'gbe/modal_performer_form.tmpl'
+    success_url = reverse_lazy('home', urlconf="gbe.urls")
+    page_title = 'Stage Persona'
+    view_title = 'Tell Us About Your Stage Persona'
 
     def get_initial(self):
         initial = super().get_initial()
@@ -16,6 +21,12 @@ class PerformerCreate(PermissionRequiredMixin, AccessMixin, CreateView):
 
     def has_permission(self):
         return hasattr(self.request.user, 'profile')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = self.page_title
+        context['view_title'] = self.view_title
+        return context
 
 class PerformerUpdate(PermissionRequiredMixin, UpdateView):
     model = Performer
