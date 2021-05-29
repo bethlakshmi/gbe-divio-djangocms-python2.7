80fc6dce27e3347611951890a4f3ccc77a9e9982
Betty Blaize <bethlakshmi@gmail.com>
2021-05-29 11:46:11 -0400

    Long list gone.  Fixed some lameness in view costume, instead

--- gbe/views/view_costume_view.py
+++ gbe/views/view_costume_view.py
@@ -3,6 +3,7 @@ from gbe.views import ViewBidView
 from gbe.forms import (
     CostumeBidSubmitForm,
     CostumeDetailsSubmitForm,
+    PersonaForm,
 )
 from gbe.models import Costume
 
@@ -11,11 +12,16 @@ class ViewCostumeView(ViewBidView):
     bid_type = Costume
     object_form_type = CostumeBidSubmitForm
     object_detail_form_type = CostumeDetailsSubmitForm
+    bidder_form_type = PersonaForm
     viewer_permissions = ('Costume Reviewers',)
     bid_prefix = ''
+    performer_prefix = "The Performer"
 
     def get_display_forms(self):
         bid_form = self.object_form_type(instance=self.bid,
                                          prefix=self.bid_prefix)
         detail_form = self.object_detail_form_type(instance=self.bid)
+        if self.bid.performer:
+            self.performer = self.bid.performer
         return (bid_form, detail_form)
+
