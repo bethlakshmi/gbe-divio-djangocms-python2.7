2fbfb0d9bc14c9ef2af2d7599489e5bf7f1a98ce
Betty Blaize <bethlakshmi@gmail.com>
2021-08-14 13:03:00 -0400

    refactor bptevent_edit and start changing branding from BPT to ticketing

--- ticketing/views/__init__.py
+++ ticketing/views/__init__.py
@@ -1,10 +1,10 @@
 from .remains import (
-    bptevent_edit,
     delete_ticket_item,
     index,
     set_ticket_to_event,
     ticket_item_edit,
 )
+from .ticket_event_edit import ticket_event_edit
 from .ticket_items import ticket_items
 from .transactions import transactions
 from .create_transaction import CreateTransaction
