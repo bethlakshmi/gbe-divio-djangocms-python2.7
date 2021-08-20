d1fa79eaf8a5e55d0ce374bb98c230f3769d33eb
Betty Blaize <bethlakshmi@gmail.com>
2021-08-19 20:06:43 -0400

    fix skeleton

--- ticketing/views/checklistitem_list.py
+++ ticketing/views/checklistitem_list.py
@@ -1,12 +1,10 @@
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import PermissionRequiredMixin
-from gbe.models import Conference
 from ticketing.models import (
     CheckListItem,
     RoleEligibilityCondition,
-    TicketingEligibilityCondition,
-    TicketingEvents,
     TicketItem,
+    TicketingEvents,
 )
 from django.db.models import Count
 
@@ -19,10 +17,8 @@ class CheckListItemList(PermissionRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        conferences = Conference.objects.exclude(status="completed")
-        ticket_conditions = {}
         context['tickets'] = TicketItem.objects.exclude(
-            ticketing_event__conference__in=conferences).exclude(
+            ticketing_event__conference__status="completed").exclude(
             ticketing_event__act_submission_event=True).exclude(
             ticketing_event__vendor_submission_event=True).order_by(
             'ticketing_event__title', 'title')
@@ -33,5 +29,4 @@ class CheckListItemList(PermissionRequiredMixin, ListView):
             act_submission_event=True).exclude(
             vendor_submission_event=True).annotate(
             count_items=Count('ticketitems')).order_by('title')
-
         return context
