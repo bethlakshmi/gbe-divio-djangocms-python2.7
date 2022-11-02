d093ea7394caeba59166ecfb66c0aa5c9cd42eb8
Betty Blaize <bethlakshmi@gmail.com>
2022-11-02 10:03:44 -0400

    Do flexible header and title for view

--- gbe/views/view_class_view.py
+++ gbe/views/view_class_view.py
@@ -15,8 +15,9 @@ class ViewClassView(ViewBidView):
     owner_prefix = 'The Teacher(s)'
 
     def make_context(self):
-        context = {'performer': self.bid.teacher,
+        context = self.get_messages()
+        context.update({'performer': self.bid.teacher,
                    'class': self.bid,
                    'scheduling_info': get_scheduling_info(self.bid),
-                   'display_contact_info': True}
+                   'display_contact_info': True})
         return context
