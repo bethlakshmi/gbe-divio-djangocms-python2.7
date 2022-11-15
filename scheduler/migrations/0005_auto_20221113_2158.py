dded55993092f2db679c96fc82f59f0cc8560782
Betty Blaize <bethlakshmi@gmail.com>
2022-11-14 20:08:33 -0500

    gnarly bug

--- scheduler/migrations/0005_auto_20221113_2158.py
+++ scheduler/migrations/0005_auto_20221113_2158.py
@@ -14,7 +14,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='event',
             name='parent',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='scheduler.event'),
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.CASCADE,
+                to='scheduler.event',
+                related_name='children'),
         ),
         migrations.AddField(
             model_name='event',
