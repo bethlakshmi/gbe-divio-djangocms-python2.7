a17c0277a0e0cee13cbb0d4956fa01873851fca7
bethlakshmi <bethlakshmi@gmail.com>
2023-01-06 19:11:49 -0500

    GBE-359: Fix bug in volunteer create (#360)
    * fixed, and tested
    
    * test both paths, remove print

--- gbe/scheduling/forms/volunteer_opportunity_form.py
+++ gbe/scheduling/forms/volunteer_opportunity_form.py
@@ -1,9 +1,17 @@
-from django.forms import BooleanField
+from django.forms import (
+    BooleanField,
+    CharField,
+    HiddenInput,
+)
 from gbe.scheduling.forms import ScheduleBasicForm
 
 
 class VolunteerOpportunityForm(ScheduleBasicForm):
     approval = BooleanField(initial=False, required=False)
+    event_style = CharField(
+        widget=HiddenInput(),
+        required=True,
+        initial="Volunteer")
 
     class Meta:
         fields = ['title',
