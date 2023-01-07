fa6a0c6441c72b658b9ac0a0c64e0ab551013a58
Betty Blaize <bethlakshmi@gmail.com>
2023-01-06 19:11:19 -0500

    test both paths, remove print

--- tests/gbe/scheduling/test_edit_show_view.py
+++ tests/gbe/scheduling/test_edit_show_view.py
@@ -92,11 +92,15 @@ class TestEditShowWizard(TestCase):
             response,
             'class="panel-collapse collapse show"',
             3)
-        print(response.content)
         self.assertContains(
             response,
             '<input type="hidden" name="new_opp-event_style" value="Volunteer"'
             ' id="id_new_opp-event_style">',
+            html=True)
+        self.assertContains(
+            response,
+            '<input type="hidden" name="new_slot-event_style" value='
+            '"Rehearsal Slot" id="id_new_slot-event_style">',
             html=True)
 
     def test_good_user_get_rehearsal_w_acts(self):
