417323a54b59700859eb8de8dc1529673ffa43ba
Betty Blaize <bethlakshmi@gmail.com>
2023-01-30 21:09:02 -0500

    Fix migrations after merge

--- gbe/migrations/0029_emailtemplatesender_from_name.py
+++ gbe/migrations/0029_emailtemplatesender_from_name.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('gbe', '0025_remove_performer_homepage'),
+        ('gbe', '0028_remove_techinfo_pronouns'),
     ]
 
     operations = [
