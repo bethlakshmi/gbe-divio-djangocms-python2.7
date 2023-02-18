349c83db1c426bc22533bc0f6e7af77b5fb9e4d5
bethlakshmi <bethlakshmi@gmail.com>
2023-02-18 09:13:16 -0500

    Gbe 344:  Fast end to social media work (#396)
    * so far so good
    
    * solider
    
    * bid review
    
    * start of testing
    
    * tests for all places it shows up
    
    * Update test_review_act.py
    
    * Create the report
    
    * tests

--- gbe/reporting/views/__init__.py
+++ gbe/reporting/views/__init__.py
@@ -7,3 +7,4 @@ from .staff_area_view import staff_area_view
 from .all_volunteer_view import all_volunteer_view
 from .welcome_letter_view import WelcomeLetterView
 from .performer_show_comp_view import PerformerShowComp
+from .performer_slides_list import PerformerSlidesList
