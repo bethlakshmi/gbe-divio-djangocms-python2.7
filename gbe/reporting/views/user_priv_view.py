52930e359303113ba47b5075cdf0e8e00af470f8
Betty Blaize <bethlakshmi@gmail.com>
2023-04-23 09:59:55 -0400

    got missing booked & staff users

--- gbe/reporting/views/user_priv_view.py
+++ gbe/reporting/views/user_priv_view.py
@@ -5,12 +5,15 @@ from django.contrib.auth.models import (
     User,
 )
 from django.db.models import Q
+from gbe.models import StaffArea
 from gbe.functions import (
+    conference_slugs,
     get_current_conference,
 )
 from gbetext import privileged_event_roles
 from collections import OrderedDict
 from django.contrib import messages
+from scheduler.idd import get_people
 
 
 class UserPrivView(GbeContextMixin, ListView):
@@ -19,7 +22,7 @@ class UserPrivView(GbeContextMixin, ListView):
     queryset = User.objects.exclude(groups=None)
     page_title = 'User Privilege Report'
     view_title = 'Users with Special Privileges'
-    intro_text = 'This is all users that currently have privileges in the site.'
+    intro_text = 'This is all users that have privileges in the site.'
 
     def get_context_data(self, **kwargs):
         from gbe.special_privileges import special_menu_tree
@@ -53,14 +56,29 @@ class UserPrivView(GbeContextMixin, ListView):
                             self.request,
                             "group: %s - not a configured group" % (group))
                     elif node['parent_id'] in group_power[group].keys():
-                        group_power[group][
-                            node['parent_id']]['children'] += [node['title']]
+                        if node['title'] not in group_power[group][node[
+                            'parent_id']]['children']:
+                            group_power[group][node['parent_id']][
+                                'children'] += [node['title']]
                     else:
                         messages.error(
                             self.request,
                             "menu: %s, group: %s - parent not found" % (
                                 node['title'],
                                 group))
-
         context['group_power'] = group_power
+
+        context['conf_priv_users'] = []
+        response = get_people(
+            labels=[conference_slugs(current=True)],
+            roles=privileged_event_roles)
+        for person in response.people:
+            if not self.queryset.filter(pk=person.user.pk).exists():
+                context['conf_priv_users'] += [person.user]
+        for area in StaffArea.objects.exclude(conference__status="completed"):
+            if not self.queryset.filter(
+                    pk=area.staff_lead.user_object.pk).exists() and (
+                    area.staff_lead.user_object not in context[
+                        'conf_priv_users']):
+                context['conf_priv_users'] += [area.staff_lead.user_object]
         return context
