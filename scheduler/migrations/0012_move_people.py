cc07a361753a05a5ce9dbd07fa9d1386c6faffad
Betty Blaize <bethlakshmi@gmail.com>
2023-06-06 11:31:48 -0400

    sync

--- scheduler/migrations/0012_move_people.py
+++ scheduler/migrations/0012_move_people.py
@@ -97,7 +97,7 @@ def migrate_people(apps, schema_editor):
             p_alloc = PeopleAllocation(event=alloc.event,
                                        people=people,
                                        label=label_text,
-                                       role=worker.role)
+                                       role=worker.role.capitalize())
             p_alloc.save()
             if Ordering.objects.filter(allocation=alloc).exists():
                 ordering = Ordering.objects.get(allocation=alloc)
