c136655efcfc2337c21379a5aa0a3d5269262456
bethlakshmi <bethlakshmi@gmail.com>
2023-12-19 12:46:46 -0500

    working ticket link

--- ticketing/functions.py
+++ ticketing/functions.py
@@ -3,11 +3,14 @@ from datetime import datetime
 from ticketing.models import (
     TicketingEvents,
     TicketItem,
+    TicketPackage,
+    TicketType,
 )
 from ticketing.eventbrite import import_eb_ticket_items
 from ticketing.humantix import HumantixClient
 from ticketing.brown_paper import import_bpt_ticket_items
 from gbetext import class_styles
+from django.db.models import Q
 
 
 def import_ticket_items():
@@ -25,29 +28,63 @@ def import_ticket_items():
 
 
 def get_tickets(linked_event):
+    # note - the old way didn't seem to care about start/end dates?
+    # the new Humanitix way DOES so we can use that when the API lets us.
     general_events = []
+    package_query = TicketPackage.objects.filter(
+        ticketing_event__conference__conference_slug__in=linked_event.labels,
+        live=True,
+        has_coupon=False).exclude(
+        start_time__gt=datetime.now()).exclude(
+        end_time__lt=datetime.now())
+    packages = []
+    link = None
     if linked_event.event_style not in ["Master", "Volunteer"]:
         general_events = TicketingEvents.objects.filter(
             include_most=True,
-            conference__conference_slug__in=linked_event.labels)
+            conference__conference_slug__in=linked_event.labels).exclude(
+            source=3)
+        packages = package_query.filter(whole_shebang=True)
     if linked_event.event_style in class_styles:
         general_events = list(chain(
             general_events,
             TicketingEvents.objects.filter(
                 include_conference=True,
-                conference__conference_slug__in=linked_event.labels)))
+                conference__conference_slug__in=linked_event.labels))).exclude(
+            source=3)
+
+        packages = package_query.filter(
+            Q(conference_only_pass=True) | Q(whole_shebang=True))
 
     general_events = list(chain(
         general_events,
         TicketingEvents.objects.filter(
-            linked_events=linked_event)))
+            linked_events=linked_event).exclude(
+            source=3)))
 
     ticket_events = []
 
     for event in general_events:
         if event.live_ticket_count > 0 and event not in ticket_events:
             ticket_events += [event]
-    return ticket_events
+
+    tickets = TicketType.objects.filter(
+        linked_events=linked_event,
+        live=True,
+        has_coupon=False).exclude(
+        start_time__gt=datetime.now()).exclude(
+        end_time__lt=datetime.now())
+
+    # with Humantix, there is only 1 event, so all links are the same
+    if packages.count() > 0:
+        link = packages.first().ticketing_event.link
+    elif tickets.count() > 0:
+        link = tickets.first().ticketing_event.link
+
+    return {"events": ticket_events,
+            "link": link,
+            "tickets": tickets,
+            "packages": packages}
 
 
 def get_fee_list(bid_type, conference):
