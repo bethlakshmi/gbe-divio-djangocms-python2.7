486be32f8b715b3b380dbf21f407c70664ab122e
bethlakshmi <bethlakshmi@gmail.com>
2023-12-30 19:01:54 -0500

    fixes 2 places

--- gbe_utils/mixins/form_to_table_mixin.py
+++ gbe_utils/mixins/form_to_table_mixin.py
@@ -4,6 +4,9 @@ from gbe_utils.mixins import GbeFormMixin
 class FormToTableMixin(GbeFormMixin):
 
     def get_success_url(self):
+        next_url = self.request.GET.get('next', self.success_url)
+        if "?" in next_url:
+            next_url = next_url[:next_url.index("?")]
         return "%s?changed_id=%d" % (
-            self.request.GET.get('next', self.success_url),
+            next_url,
             self.object.pk)
